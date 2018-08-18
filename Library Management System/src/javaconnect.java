fd8e737148e2efafc08f6156abe5e130576aaf50
Zibran Khan <Zibran Khan@DESKTOP-VM605H3>
2018-08-18 15:53:52 +0530

    This is a Library Management System created Using JavaSwing and developed by Zibran Khan

---
+++ Library Management System/src/javaconnect.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Zibran Khan
+ */
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class javaconnect
+{
+    Connection conn;
+    public static Connection ConnecrDb()
+    {
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Zibran Khan\\Documents\\NetBeansProjects\\Library Management System\\LibraryNew.sqlite");
+                return conn;
+            
+        }catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null ,e);
+            return null;
+        }
+    }
+    
+    
+}
